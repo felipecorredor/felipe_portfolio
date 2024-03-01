a571fd1ead3a6a54c16feba008bee5f8d5095442
Felipe Corredor <pipecorredor06@gmail.com>
2024-03-01 16:53:40 -0500

    update computers canva

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,8 +27,8 @@ const Hero = () => {
         </div>
       </div>
 
-      {/* <ComputersCanvas /> */}
-      <SpaceManCanvas />
+      <ComputersCanvas />
+      {/* <SpaceManCanvas /> */}
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
